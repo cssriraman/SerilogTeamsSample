d742d9a80a2bee8ceae947763d5336461355c85e
Sriraman <cssriraman@gmail.com>
2019-10-15 00:21:18 +0530

    Added Asp.net core 2.2 web api project Added Serilog Added Serilog.sink.console Added Serilog. Microsoft teams support

---
+++ src/SerilogTeamsSample/SerilogTeamsSample/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Serilog;
+
+namespace SerilogTeamsSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            Serilog.Debugging.SelfLog.Enable(Console.WriteLine);
+
+            Log.Debug("Configure starts ...");
+            if (env.IsDevelopment())
+            {
+                Log.Debug("IsDevelopment = true");
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
